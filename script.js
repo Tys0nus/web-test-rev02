b0b967a6f32dfce4aeaf2cb19bfec1ee2c9865fd
Aditya Chaugule <adityachaugule1997@gmail.com>
2023-12-28 03:41:47 -0500

    --update

--- script.js
+++ script.js
@@ -2,45 +2,56 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 let scene, camera, renderer, cube, controls;
+let raycaster, mouse;
+
 
 function init() {
-    // Create a scene
     scene = new THREE.Scene();
-
-    // Create a camera
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    raycaster = new THREE.Raycaster();
+    mouse = new THREE.Vector2();
 
-    // Create a renderer
     renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-    // Create a cube
-    const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    cube = new THREE.Mesh(geometry, material);
+    // const geometry = new THREE.BoxGeometry();
+    const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+    // const material = new THREE.MeshBasicMaterial({ color: 0x00ffff});
+    const material = new THREE.PointsMaterial({
+      size: 0.01
+    })
+    cube = new THREE.Points(geometry, material);
     scene.add(cube);
-
-    // Position the camera
+    
     camera.position.z = 5;
 
-    // Add OrbitControls
     controls = new OrbitControls(camera, renderer.domElement);
+    const size = 10;
+    const divisions = 10;
+    const gridHelper = new THREE.GridHelper(size, divisions);
+    scene.add(gridHelper);
+    window.addEventListener('mousemove', onMouseMove, false);
+    
 }
 
 function animate() {
     requestAnimationFrame(animate);
 
-    // Rotate the cube
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    // cube.rotation.x += 0.01;
+    cube.rotation.y += 0.005;
 
-    // Update the controls
     controls.update();
 
-    // Render the scene
     renderer.render(scene, camera);
 }
+
+function onMouseMove(event) {
+  // Convert to normalized device coordinates
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
+}
+
 
 init();
 animate();
